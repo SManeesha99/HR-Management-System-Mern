9e47134cef355341422e4180dc5827f29327a869
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-08-16 15:04:45 +0530

    commit

--- Project/client/src/components/AddEmployee.js
+++ Project/client/src/components/AddEmployee.js
@@ -1,11 +1,11 @@
 import React, { useState, Component } from "react";
-// import "../css/form.css";
+import "./css/form.css";
 import axios from "axios";
 
 export default class AddEmployee extends Component {
 
 
-  constructor(props) {  
+  constructor(props) {
     super(props);
     this.state = {
       employee: [],
@@ -64,72 +64,73 @@ export default class AddEmployee extends Component {
 
   render() {
     return (
-      <div className="container1">
+      <div className="containerForm">
 
         <form className="create" onSubmit={this.onSubmit}>
           <h3>Add New Employee</h3>
-
-          <label>Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-          />
-
-          <label>NIC: </label>
-          <input
-            type="text"
-            name="id"
-            value={this.state.id}
-            onChange={this.handleChange}
-            required
-          />
-
-          <label>Email: </label>
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
-
-          <label>Gender: </label>
-          <select
-            name="gender"
-            value={this.state.gender}
-            onChange={this.handleChange}
-            required
-          >
-            <option value="">--Select Gender--</option>
-            <option value="Male">M</option>
-            <option value="Female">F</option>
-          </select>
-
-          <label>Type: </label>
-          <input
-            type="tel"
-            name="type"
-            value={this.state.type}
-            onChange={this.handleChange}
-            
-          />
-
-
-          <label>Salary: </label>
-          <input
-            type="number"
-            name="salary"
-            value={this.state.salary}
-            onChange={this.handleChange}
-            required></input>
-
           <center>
-            <button className="btn" type="submit">
-              Submit
-            </button>
+            <label>Name: </label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+
+            <label>NIC: </label>
+            <input
+              type="text"
+              name="id"
+              value={this.state.id}
+              onChange={this.handleChange}
+              required
+            />
+
+            <label>Email: </label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+
+            <label>Gender: </label>
+            <select
+              name="gender"
+              value={this.state.gender}
+              onChange={this.handleChange}
+              required
+            >
+              <option value="">--Select Gender--</option>
+              <option value="Male">M</option>
+              <option value="Female">F</option>
+            </select>
+
+            <label>Type: </label>
+            <input
+              type="tel"
+              name="type"
+              value={this.state.type}
+              onChange={this.handleChange}
+
+            />
+
+
+            <label>Salary: </label>
+            <input
+              type="number"
+              name="salary"
+              value={this.state.salary}
+              onChange={this.handleChange}
+              required></input>
+
+            <center>
+              <button className="btn" type="submit">
+                Submit
+              </button>
+            </center>
           </center>
         </form>
       </div>
