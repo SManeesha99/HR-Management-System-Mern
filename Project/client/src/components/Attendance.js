a0dc98129a5f72e1331b163845e55a3d3e209ff7
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-08-17 13:32:52 +0530

    commit

--- Project/client/src/components/Attendance.js
+++ Project/client/src/components/Attendance.js
@@ -1,62 +1,186 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
 import Swal from 'sweetalert2';
 
-function Attendance() {
-    const params = useParams();
-    const [employee, setEmployee] = useState([]);
+function withParams(Component) {
+    return props => <Component params={
+        useParams()
+    } />
+}
 
-    useEffect(() => {
-        retrievePosts();
-    }, []);
+class AdminDelivery extends Component {
 
-    const retrievePosts = () => {
+    constructor(props) {
+        super(props);
+
+        this.attendance = "";
+
+        this.state = {
+            id: props.params.id,
+            employee: [],
+            searchKey: "",
+        };
+
+    }
+
+    componentDidMount() {
+        this.retrievePosts();
+
+    }
+
+    retrievePosts() {
         axios.get("/employee/post").then(res => {
             if (res.data.success) {
-                setEmployee(res.data.existingPosts);
-                console.log(employee);
+                this.setState({ employee: res.data.existingPosts });
+                console.log(this.state.employee)
             }
         });
     }
 
-    return (
-        <div>
-            <div className='mt-5'>
-                <div className="container3">
-                    <div className="add_btn mt-2 mb-2"></div>
-                    <section class="main">
-                        <div class="main-top">
-                            <h1>Employee List</h1>
-                        </div>
-                        <table class="table" id="EmployeeTable">
-                            <thead>
-                                <th scope="col" >No.</th>
-                                <th scope="col" >Name</th>
-                                <th scope="col" >Attendance</th>
-                            </thead>
-                            <tbody>
-                                {employee.map((employee, index) => (
-                                    <tr key={index}>
-                                        <th scope="row">{index + 1}</th>
-                                        <td>{employee.name}</td>
-                                        <td>{employee.attendance}</td>
-                                        <center>
-                                            <td>
-                                                <a href={`/update/${employee._id}`} className="btn-edit">Update</a>
-                                            </td>
-                                            
-                                        </center>
+
+
+    // edit
+    handleChange = (e) => {
+        const { name, value } = e.target;
+
+        this.setState({
+            ...this.state,
+            [name]: value
+        });
+        this.attendance = value;
+    }
+
+    onSave = (id) => {
+        let data = this.state.employee.filter((post) => post._id === id)[0];
+        data.attendance = this.attendance;
+    
+        axios.put(`/employee/post/${id}`, data).then((res) => {
+            if (res.data.success) {
+                Swal.fire({
+                    title: 'Updated Successfully!',
+                    text: 'Your changes have been saved.',
+                    icon: 'success',
+                    showConfirmButton: false, // Hide the "OK" button
+                    timer: 3000, // Automatically close after 3 seconds
+                }).then(() => {
+                    this.setState({
+                        name: "",
+                        attendance: ""
+                    });
+                });
+            }
+        });
+    };
+    
+
+
+    //search part
+    handleSearchKeyChange = (e) => {
+        const searchKey = e.currentTarget.value;
+        this.setState({ searchKey });
+        this.filterData(this.state.employee, searchKey);
+    };
+
+    filterData(employee, searchkey) {
+        const result = employee.filter((post) =>
+            post.name.toLowerCase().includes(searchkey.toLowerCase())
+        );
+        this.setState({ employee: result });
+    }
+
+    resetSearch = () => {
+        this.setState({ searchKey: "" }, () => {
+            this.retrievePosts();
+        });
+    };
+
+    render() {
+        const { searchKey } = this.state;
+        const filteredDelivery = this.state.employee.filter((employee) =>
+            employee.name.toLowerCase().includes(searchKey.toLowerCase())
+        );
+        return (
+            <div>
+                <div className='mt-5'>
+                    <div className="container">
+                       <form className="form-inline my-2 my-lg-9 ml-auto">
+                            <input
+                                className="form-control"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                                value={searchKey}
+                                onChange={this.handleSearchKeyChange}
+                            />
+                            <button
+                                className="btn btn-outline-success my-2 my-sm-0"
+                                type="button"
+                                onClick={this.resetSearch}
+                            >
+                                Reset
+                            </button>
+                        </form>
+                        <div className="table-responsive">
+                            <table className="table" id="deliveryTable">
+                                <thead>
+                                    <tr className="table-dark">
+                                        <th scope="col"></th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Attendance</th>
+                                        <th></th>
+                                        <th></th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        
-                    </section>
+                                </thead>
+                                <tbody>
+                                    {this.state.employee.map((employee, index) => (
+                                        <tr key={index}>
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{employee.name}</td>
+                                            <td>
+                                                <input type="text" class="form-control"
+                                                    value={
+                                                        this.state.attendance
+                                                    }
+                                                    onChange={
+                                                        this.handleChange
+                                                    }
+                                                    id="formGroupExampleInput"
+                                                    placeholder={
+                                                        employee.attendance
+                                                    } /></td>
+
+
+
+
+
+                                            <td onClick={() => {
+                                                this.onSave(employee._id);
+                                                setTimeout(() => {
+                                                    window.location.reload(); 
+                                                }, 1200);
+                                            }}>
+                                                <a className="btn btn-success">
+                                                    <i className="fas fa-edit"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    ))
+                                    } </tbody>
+
+
+                            </table>
+
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
 
-export default Attendance;
+export default withParams(AdminDelivery);
