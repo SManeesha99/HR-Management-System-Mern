bf64cdd543de1fce7944df2fe117107a237e28c3
AsithaManupiya2000 <asithamanupiya@gmail.com>
2023-08-19 03:47:06 +0530

    Update Register.js

--- Project/client/src/components/Register.js
+++ Project/client/src/components/Register.js
@@ -4,77 +4,100 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function Register() {
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [secretKey, setSecretKey] = useState(""); // New state for secret key
+    const navigate = useNavigate();
+
+    const secretKeyToMatch = "hrmanager"; // Replace with your actual secret key
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        
+        if (secretKey !== secretKeyToMatch) {
+            alert("Invalid secret key. Registration is not allowed.");
+            return;
+        }
+        
         axios.post("/register", { name, email, password })
             .then(result => {
-                console.log(result)
-                navigate("/login")
+                console.log(result);
+                navigate("/login");
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
     }
-
 
     return (
         <div>
             <h1>Register</h1>
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label htmlFor="email">
+                    <label htmlFor="name">
                         <strong>Name</strong>
                     </label>
-                    <input type="text"
+                    <input
+                        type="text"
                         className="form-control rounded-0"
                         autoComplete="off"
                         name="name"
                         placeholder="Enter your name"
-                        onChange={(e) => setName(e.target.value)} />
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="email">
                         <strong>Email</strong>
                     </label>
-                    <input type="email"
+                    <input
+                        type="email"
                         className="form-control"
                         autoComplete="off"
                         name="email"
                         placeholder="Enter your email"
-                        onChange={(e) => setEmail(e.target.value)} />
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="email">
+                    <label htmlFor="password">
                         <strong>Password</strong>
                     </label>
-                    <input type="password"
+                    <input
+                        type="password"
                         className="form-control"
                         autoComplete="off"
                         name="password"
                         placeholder="Enter your password"
-                        onChange={(e) => setPassword(e.target.value)} />
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="secretKey">
+                        <strong>Secret Key</strong>
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        autoComplete="off"
+                        name="secretKey"
+                        placeholder="Enter the secret key"
+                        onChange={(e) => setSecretKey(e.target.value)}
+                    />
                 </div>
 
                 <button type="submit" className="btn btn-primary">
                     Register
                 </button>
             </form>
-            <p>Already have a account</p>
-            <Link to="/login" type="submit" className="btn btn-primary">
+            <p>Already have an account?</p>
+            <Link to="/login" className="btn btn-primary">
                 Login
             </Link>
-
-
-
-
         </div>
-
-    )
+    );
 }
 
-export default Register
+export default Register;
