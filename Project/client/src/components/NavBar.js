45cf98c2d0e754ad3d01077fedafa86149de952b
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2023-08-27 09:20:02 +0530

    commit

--- Project/client/src/components/NavBar.js
+++ Project/client/src/components/NavBar.js
@@ -1,43 +1,25 @@
-import React from 'react'
-import './css/navbar.css';
+import React from 'react';
+import './css/navbar.css'; 
 
-export default function Navbar() {
+export default function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
+            <img src="../images/logo.png" alt="Logo" className="sidebar-logo" />
 
-
-
-<nav class="navbar navbar-expand-lg">
-        <div class="container">
-           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <img src="../images/logo.png"  width="70" height="70"/>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/details">Employee Details</a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="/attendance">Employee Attendance</a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="/viewAttendance">View Attendance</a>
-              </li> 
-             
-              
-              
-                     
+            <ul className="sidebar-nav">
+                <li className="sidebar-item">
+                    <a href="/" className="sidebar-link">Home</a>
+                </li>
+                <li className="sidebar-item">
+                    <a href="/details" className="sidebar-link">Employee Details</a>
+                </li>
+                <li className="sidebar-item">
+                    <a href="/attendance" className="sidebar-link">Employee Attendance</a>
+                </li>
+                <li className="sidebar-item">
+                    <a href="/viewAttendance" className="sidebar-link">View Attendance</a>
+                </li>
             </ul>
-          </div>
         </div>
-      </nav>
-
-
-        </div>
-    )
+    );
 }
-
