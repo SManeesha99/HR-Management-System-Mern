be6c14b81eb63b8709cffec19a26d9676a73bf74
vidura-chahuranga <vidurachathuranga2020@gmail.com>
2023-09-27 03:50:34 +0530

    fixed marking multiple attendance in a day
    Set all the fields in marking attendance form into read-only and fixed the issue that user can marking attendance multiple times for a particular day.

--- Project/client/src/components/AddAttendance.js
+++ Project/client/src/components/AddAttendance.js
@@ -1,109 +1,134 @@
-import React, { useState , useEffect  } from 'react';
-import axios from 'axios';
-import EmpSideNav from './EmpSideNav'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EmpSideNav from "./EmpSideNav";
+import { useNavigate } from "react-router-dom";
 
 const AddAttendance = () => {
+  const [empNo, setEmpNo] = useState("");
+  const [empName, setEmpName] = useState("");
+  const [date, setDate] = useState("");
+  const [checkIn, setCheckIn] = useState("");
 
-     const [empNo, setEmpNo] = useState('');
-     const [empName, setEmpName] = useState('');
-    const [date, setDate] = useState('');
-    const [checkIn, setCheckIn] = useState('');
+  // useNavigate hook
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const currentDate = new Date();
-        const formattedDate = currentDate.toISOString().split('T')[0];
-        const formattedTime = currentDate.toTimeString().slice(0, 5);
+ 
+  useEffect(() => {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString("en-CA");
+    const formattedTime = currentDate.toTimeString().slice(0, 5);
 
-        setDate(formattedDate);
-        setCheckIn(formattedTime);
+    setDate(formattedDate);
+    setCheckIn(formattedTime);
 
-        // Retrieve empNo from local storage
-        const storedEmpName = localStorage.getItem('empName');
-        if (storedEmpName) {
-            setEmpName(storedEmpName);
+    // Retrieve empNo from local storage
+    const storedEmpName = localStorage.getItem("empName");
+    if (storedEmpName) {
+      setEmpName(storedEmpName);
+    }
+    const storedEmpNo = localStorage.getItem("empNo");
+    if (storedEmpNo) {
+      setEmpNo(storedEmpNo);
+    }
+
+    // checking exisiting attendances
+    const checkMarkedIn = async (employeeNumber) => {
+        const response = await axios.get(
+          `http://localhost:8000/attendance/check/${employeeNumber}`
+        );
+    
+        if(response.data.status){
+            navigate("/ownAttendance");
         }
-        const storedEmpNo = localStorage.getItem('empNo');
-        if (storedEmpNo) {
-            setEmpNo(storedEmpNo);
+      };
+
+    //   calling to checking existing attendace function
+      checkMarkedIn(storedEmpNo);
+  }, []);
+
+  const handleRecordAttendance = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/attendance/add",
+        {
+          empNo,
+          attendance: [{ date, checkIn }],
         }
-    }, []);
+      );
 
-    const handleRecordAttendance = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8000/attendance/add', {
-                empNo,
-                attendance: [{ date, checkIn }]
-            });
+      alert("Attendance recorded:", response.data);
+      window.location.href = "/ownAttendance";
+    } catch (error) {
+      console.error("Error recording attendance:", error);
+    }
+  };
 
-            alert('Attendance recorded:', response.data);
-            window.location.href = '/ownAttendance';
-        } catch (error) {
-            console.error('Error recording attendance:', error);
-        }
-    };
-
-    return (
-        <div>
-            <EmpSideNav />
-            <div className='py-5'>
-    <div className="container">
-        <div className="row justify-content-center">
+  return (
+    <div>
+      <EmpSideNav />
+      <div className="py-5">
+        <div className="container">
+          <div className="row justify-content-center">
             <div className="col-md-5">
-                <div className="card">
-                    <div className="card-header">
-                        <h3>Add Daily ON</h3>
+              <div className="card">
+                <div className="card-header">
+                  <h3>Add Daily ON</h3>
+                </div>
+                <div className="card-body text-center">
+                  <form onSubmit={handleRecordAttendance}>
+                    <div className="form-group">
+                      <label htmlFor="empName">Employee Name</label>
+                      <input
+                        type="text"
+                        id="empName"
+                        value={empName}
+                        onChange={(e) => setEmpName(e.target.value)}
+                        className="form-control"
+                        required
+                        readOnly
+                      />
                     </div>
-                    <div className="card-body text-center">
-                        <form onSubmit={handleRecordAttendance}>
-                        <div className="form-group">
-                                <label htmlFor="empName">Employee Name</label>
-                                <input
-                                    type="text"
-                                    id="empName"
-                                    value={empName}
-                                    onChange={(e) => setEmpName(e.target.value)}
-                                    className="form-control"
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="empNo">Employee No</label>
-                                <input
-                                    type="text"
-                                    id="empNo"
-                                    value={empNo}
-                                    onChange={(e) => setEmpNo(e.target.value)}
-                                    className="form-control"
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="date">Date</label>
-                                <input
-                                    type="date"
-                                    id="date"
-                                    value={date}
-                                    onChange={(e) => setDate(e.target.value)}
-                                    className="form-control"
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="checkIn">ON Time</label>
-                                <div className="d-flex align-items-center justify-content-center">
-                                    <input
-                                        type="time"
-                                        id="checkIn"
-                                        value={checkIn}
-                                        onChange={(e) => setCheckIn(e.target.value)}
-                                        className="form-control"
-                                        required
-                                    />
-                                    {/* <button type="button" className="btn btn-primary ml-3">ON</button> */}
-                                </div>
-                            </div>
-                            {/* <div className="form-group">
+                    <div className="form-group">
+                      <label htmlFor="empNo">Employee No</label>
+                      <input
+                        type="text"
+                        id="empNo"
+                        value={empNo}
+                        onChange={(e) => setEmpNo(e.target.value)}
+                        className="form-control"
+                        required
+                        readOnly
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="date">Date</label>
+                      <input
+                        type="date"
+                        id="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        className="form-control"
+                        required
+                        readOnly
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="checkIn">ON Time</label>
+                      <div className="d-flex align-items-center justify-content-center">
+                        <input
+                          type="time"
+                          id="checkIn"
+                          value={checkIn}
+                          onChange={(e) => setCheckIn(e.target.value)}
+                          className="form-control"
+                          required
+                          readOnly
+                        />
+                        {/* <button type="button" className="btn btn-primary ml-3">ON</button> */}
+                      </div>
+                    </div>
+                    {/* <div className="form-group">
                                 <label htmlFor="checkOut">Check-Out</label>
                                 <div className="d-flex align-items-center justify-content-center">
                                     <input
@@ -117,23 +142,22 @@ const AddAttendance = () => {
                                     <button type="button" className="btn btn-danger ml-3" style={{ backgroundColor:'red' }}>OFF</button>
                                 </div>
                             </div> */}
-                            <button type="submit" className="btn btn-primary mt-3">
-                                ON
-                            </button>
-                        </form>
-                        <hr />
-                                {/* <button type="button" className="btn mt-3">
+                    <button type="submit" className="btn btn-primary mt-3">
+                      ON
+                    </button>
+                  </form>
+                  <hr />
+                  {/* <button type="button" className="btn mt-3">
                                     <a href='/viewAttendance' style={{ decoration:'none' }}>View Attendance</a>
                                 </button> */}
-                            </div>
-                        </div>
-                    </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-        </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddAttendance;
--- Project/routes/attendance.js
+++ Project/routes/attendance.js
@@ -1,93 +1,96 @@
 const router = require("express").Router();
 const Attendance = require("../models/attendance");
 
-
 //add attendance
-router.post('/add', (req, res) => {
-    Attendance.create(req.body)
-      .then(attendance => res.json({ msg: 'Attendance added successfully' }))
-      .catch(err => res.status(400).json({ error: 'Unable to add this Attendance' }));
+router.post("/add", (req, res) => {
+  Attendance.create(req.body)
+    .then((attendance) => res.json({ msg: "Attendance added successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to add this Attendance" })
+    );
 });
 
-
 //get attendance
-router.get('/', (req, res) => {
-    Attendance.find()
-        .then(attendance => res.json(attendance))
-        .catch(err => res.status(400).json({ error: 'Unable to get Attendance' }));
-}
-);
+router.get("/", (req, res) => {
+  Attendance.find()
+    .then((attendance) => res.json(attendance))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to get Attendance" })
+    );
+});
 
 //get attendance by id
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-      const attendanceId = req.params.id;
-      const attendance = await Attendance.findById(attendanceId);
-      
-      if (!attendance) {
-          return res.status(404).json({ message: 'Attendance not found' });
-      }
+    const attendanceId = req.params.id;
+    const attendance = await Attendance.findById(attendanceId);
 
-      res.status(200).json(attendance);
+    if (!attendance) {
+      return res.status(404).json({ message: "Attendance not found" });
+    }
+
+    res.status(200).json(attendance);
   } catch (error) {
-      res.status(500).json({ message: 'Error fetching attendance', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error fetching attendance", error: error.message });
   }
 });
 
 //get own attendance
-router.get('/own/:empNo', (req, res) => {
+router.get("/own/:empNo", (req, res) => {
   const empNo = req.params.empNo;
 
-  Attendance.find({ empNo: empNo }) 
-    .then(attendance => res.json(attendance))
-    .catch(err => res.status(400).json({ error: 'Unable to get Attendance' }));
+  Attendance.find({ empNo: empNo })
+    .then((attendance) => res.json(attendance))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to get Attendance" })
+    );
 });
-
 
 //update attendance
-router.put('/update/:id', (req, res) => {
-    Attendance.findByIdAndUpdate(req.params.id, req.body)
-      .then(attendance => res.json({ msg: 'Updated successfully' }))
-      .catch(err =>
-        res.status(400).json({ error: 'Unable to update the Database' })
-      );
-  });
-
-
-
-
-
-  router.put('/:id/checkout', async (req, res) => {
-    try {
-        const attendanceId = req.params.id;
-        const newCheckOut = req.body.checkOut; // Assuming you send this in the request body
-
-        const attendance = await Attendance.findById(attendanceId);
-
-        if (!attendance) {
-            return res.status(404).json({ message: 'Attendance not found' });
-        }
-
-        attendance.attendance[0].checkOut = newCheckOut; // Assuming you want to update the first attendance record
-        await attendance.save();
-
-        res.status(200).json({ message: 'Check-out time updated successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error updating check-out time', error: error.message });
-    }
+router.put("/update/:id", (req, res) => {
+  Attendance.findByIdAndUpdate(req.params.id, req.body)
+    .then((attendance) => res.json({ msg: "Updated successfully" }))
+    .catch((err) =>
+      res.status(400).json({ error: "Unable to update the Database" })
+    );
 });
 
+router.put("/:id/checkout", async (req, res) => {
+  try {
+    const attendanceId = req.params.id;
+    const newCheckOut = req.body.checkOut; // Assuming you send this in the request body
 
-router.get('/attendance/check', (req, res) => {
-    const { empNo, date } = req.query;
+    const attendance = await Attendance.findById(attendanceId);
 
-    const existingRecord = checkInRecords.find(record => record.empNo === empNo && record.date === date);
-
-    if (existingRecord) {
-        return res.json({ exists: true });
+    if (!attendance) {
+      return res.status(404).json({ message: "Attendance not found" });
     }
 
-    return res.json({ exists: false });
+    attendance.attendance[0].checkOut = newCheckOut; // Assuming you want to update the first attendance record
+    await attendance.save();
+
+    res.status(200).json({ message: "Check-out time updated successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating check-out time", error: error.message });
+  }
+});
+
+router.get("/attendance/check", (req, res) => {
+  const { empNo, date } = req.query;
+
+  const existingRecord = checkInRecords.find(
+    (record) => record.empNo === empNo && record.date === date
+  );
+
+  if (existingRecord) {
+    return res.json({ exists: true });
+  }
+
+  return res.json({ exists: false });
 });
 
 //   router.put('/update-checkout/:attendanceId/:entryId', async (req, res) => {
@@ -112,4 +115,31 @@ router.get('/attendance/check', (req, res) => {
 //     }
 // });
 
- module.exports = router; 
+// check attendance already marked or not
+router.get("/check/:id", async (req, res) => {
+  // get the employee number
+  const empId = req.params.id;
+
+  try {
+    // current date
+    const date = new Date().toLocaleDateString("en-CA");
+
+    // get existing attendance
+    const attendance = await Attendance.findOne({
+      empNo: empId,
+      "attendance.date": date,
+    });
+
+    // if attendances are exist send a response  with status TRUE 
+    if (attendance) {
+      res.json({ status: true });
+    }
+    else{
+        res.json({ status: false });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
